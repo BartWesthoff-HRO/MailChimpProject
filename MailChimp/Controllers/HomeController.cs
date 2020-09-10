6db67e1f32c3915d6d9b18b6614aacd69de9de46
BartWesthoff <55059375+BartWesthoff@users.noreply.github.com>
2020-09-10 16:31:15 +0200

    test commit

--- MailChimp/Controllers/HomeController.cs
+++ MailChimp/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace MailChimp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact.";
 
             return View();
         }
