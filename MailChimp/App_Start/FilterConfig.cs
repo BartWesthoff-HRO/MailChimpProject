79f71b22bf0dcf145c2096f2935848ed984f3357
BartWesthoff <55059375+BartWesthoff@users.noreply.github.com>
2020-09-10 16:23:42 +0200

    Add project files.

---
+++ MailChimp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MailChimp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
