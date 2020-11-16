cc5236c87ba21fafdfac57d75d39d1b28ef1fa78
BartWesthoff <0991807@hr.nl>
2020-11-16 10:49:36 +0100

    aanpassing homestyle

--- MailChimp/Controllers/ContactPersonController.cs
+++ MailChimp/Controllers/ContactPersonController.cs
@@ -15,10 +15,10 @@ namespace MailChimp.Controllers
 
             List<ContactPerson> personenLijst = new List<ContactPerson>();
 
-            personenLijst.Add(new ContactPerson() { FirstName="Bart", LastName = "Westhoff", Company = "groepje 6", Label = "werknemer" });
-            personenLijst.Add(new ContactPerson() { FirstName = "Tijmen", LastName = "Rietveld", Company = "groepje 6", Label = "werknemer" });
-            personenLijst.Add(new ContactPerson() { FirstName = "Nathan", LastName = "Samjio", Company = "groepje 6", Label = "werknemer" });
-            personenLijst.Add(new ContactPerson() { FirstName = "Isabelle", LastName = "Stienen", Company = "groepje 6", Label = "Scrummaster" });
+            personenLijst.Add(new ContactPerson() { FirstName="Bart", LastName = "Westhoff", Company = "groepje 6", Label = "werknemer", email = "bartW@email.com" });
+            personenLijst.Add(new ContactPerson() { FirstName = "Tijmen", LastName = "Rietveld", Company = "groepje 6", Label = "werknemer", email = "TijmenR@email.com" });
+            personenLijst.Add(new ContactPerson() { FirstName = "Nathan", LastName = "Samjio", Company = "groepje 6", Label = "werknemer", email = "NathanS@email.com" });
+            personenLijst.Add(new ContactPerson() { FirstName = "Isabelle", LastName = "Stienen", Company = "groepje 6", Label = "Scrummaster", email = "IsabelleS@email.com" });
             return View(personenLijst);
         }
     }
